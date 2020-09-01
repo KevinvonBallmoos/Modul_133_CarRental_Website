17bb31313a6853e2b4e3224698a48b9997cefed9
Subscaaper <kevin_mike@hispeed.ch>
2020-09-01 12:04:28 +0200

    develop commit

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    return render(request, 'functions/cars.html')
 
 
