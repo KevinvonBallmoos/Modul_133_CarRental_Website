daa03846a3a6b39de1509e115d21c7c6393fdbc7
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2020-09-30 20:50:22 +0200

    changes

--- cars/urls.py
+++ cars/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.list_cars, name='list_cars'),
     path('show_cars/<int:car_id>', views.show_cars, name='show_cars'),
     path('list_cars/<int:car_id>/delete', views.delete_cars, name='delete_cars'),
-
 ]
